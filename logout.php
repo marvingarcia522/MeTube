4f0cb29f5c91ff32b4ed3a89329264f8041bef3b
Charlie II S. Gonzales <60724141+CharlieGonzales@users.noreply.github.com>
2021-04-18 20:34:15 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,7 +5,7 @@
     $gClient->revokeToken();
 
     //Forced Hard Log off from google - then redirect to your page
-    $google_redirect_url ='https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost/test.php';
+    $google_redirect_url ='https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=test.php';
     header('Location: ' . filter_var($google_redirect_url, FILTER_SANITIZE_URL)); //redirect user back to page
     
 ?>
