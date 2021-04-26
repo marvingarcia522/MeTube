8e05bf9050c4d7bb1241dab8e0edac9b7e5e61ce
Charlie II S. Gonzales <60724141+CharlieGonzales@users.noreply.github.com>
2021-04-26 20:24:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ if ($gClient->getAccessToken()) // Sign in
     $_SESSION['familyName'] = $user['familyName'];
     $_SESSION['givenName'] = $user['givenName'];
 
-    header("location:home.php");
+    header("location:http://localhost/metube/home.php");
 
 
     } catch (Exception $e) {
